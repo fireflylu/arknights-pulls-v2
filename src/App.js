18aecf862dc195416a2578f3ce5fe2db3f71f868
fireflylu <dc.fireflylu@gmail.com>
2025-12-26 22:41:13 -0800

    fix: fit to screen, make banners scrollable

--- src/App.js
+++ src/App.js
@@ -10,6 +10,24 @@ function App() {
             image: "https://arknights.wiki.gg/images/EN_The_Rolling_Thunder_banner.png?2859d1"
         },
         {
+            name: "A",
+            characters: "a",
+            date: "dec-11-2025",
+            image: "https://arknights.wiki.gg/images/EN_The_Rolling_Thunder_banner.png?2859d1"
+        },
+        {
+            name: "A",
+            characters: "a",
+            date: "dec-11-2025",
+            image: "https://arknights.wiki.gg/images/EN_The_Rolling_Thunder_banner.png?2859d1"
+        },
+        {
+            name: "A",
+            characters: "a",
+            date: "dec-11-2025",
+            image: "https://arknights.wiki.gg/images/EN_The_Rolling_Thunder_banner.png?2859d1"
+        },
+        {
             name: "B",
             characters: "b",
             date: "jan-16-2026",
@@ -23,21 +41,23 @@ function App() {
                 <div class="split-left">
                     <h1>Hi Right</h1>
                     <p>The banner information goes here, and how many pulls per banner.</p>
-                    {banners.map((banner, index) => (
-                        <div key={index} className='split-container'>
-                            <div class="split-left">
-                                <i><img src={banner.image} alt={banner.name}></img></i>
+                    <div class="scrollbox">
+                        {banners.map((banner, index) => (
+                            <div key={index} class='banner-list'>
+                                <div class="banner-left">
+                                    <i><img src={banner.image} alt={banner.name}></img></i>
+                                </div>
+                                <div class="banner-right">
+                                    <h3><strong>{banner.name}: {banner.characters}</strong></h3>
+                                    <p><i>{banner.date}</i></p>
+                                    <form>
+                                        <label for="pulls"># of Pulls:</label>
+                                        <input type='text' id='pulls' name='pulls'></input>
+                                    </form>
+                                </div>
                             </div>
-                            <div class="split-right">
-                                <h3><strong>{banner.name}: {banner.characters}</strong></h3>
-                                <p><i>{banner.date}</i></p>
-                                <form>
-                                    <label for="pulls"># of Pulls:</label>
-                                    <input type='text' id='pulls' name='pulls'></input>
-                                </form>
-                            </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
                 <div class="split-right">
                     <h1>I'm right</h1>
